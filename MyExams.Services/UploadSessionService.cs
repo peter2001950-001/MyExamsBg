e19ce56e92064dd2c6c0e2dcc5f2838df825df84
Petar Bechev <bechev2@gmail.com>
2018-02-28 19:09:32 +0200

    Unstable version with uploadind, checking and confirmation

--- MyExams.Services/UploadSessionService.cs
+++ MyExams.Services/UploadSessionService.cs
@@ -45,5 +45,6 @@ namespace MyExams.Services
             _uploadSessionRepository.Add(uploadSession);
             _uploadSessionRepository.SaveChanges();
         }
+        
     }
 }
---
+++ MyExams/Scripts/app/teacher/indexViewModel.js
@@ -0,0 +1,28 @@
+﻿function indexViewModel(){
+    var self = this;
+    self.uploadSessionData = ko.observable();
+
+    self.uploadSessionNotification = function () {
+        $.ajax({
+            type: "get",
+            datatype: "json",
+            contenttype: "application/json",
+            url: "/t/UploadSessionNotification",
+            success: function (data) {
+                if (data.status === "HAS") {
+                    self.uploadSessionData(data);
+                    $("#uploadSessionNotification").modal("show");
+                }
+            }
+        });
+    };
+};
+
+var ivm = new indexViewModel();
+ko.applyBindings(ivm);
+$(document).ready(function () {
+    ivm.uploadSessionNotification();
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+})
